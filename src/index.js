687c9847bc23e194f947c7abb61c2f0ce046a6ce
Travis Fehr <travisefehr@gmail.com>
2022-05-17 16:53:21 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+// As a user, I should be able to:
+// 1. type a task into the input field
+// 2. click some form of a submit button
+// 3. see the task string that I provided appear in the DOM after the submit button has been activated.
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener('submit', (event) => {
+    event.preventDefault()
+    let task = document.createElement('task')
+    task.textContent = event.target["new-task-description"].value
+    document.querySelector('#tasks').append(task)
+  })
 });
